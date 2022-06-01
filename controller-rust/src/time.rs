99aa2e62c3119c6a32d7e8decc49e011dd9c2f33
Eoghanmc22 <eoghanmc@icloud.com>
2022-06-01 19:45:19 -0400

    add time system

---
+++ controller-rust/src/time.rs
@@ -0,0 +1,56 @@
+use core::cell::Cell;
+use avr_device::atmega2560::TC0;
+use avr_device::interrupt;
+use avr_device::interrupt::Mutex;
+
+// Possible Values:
+//
+// ╔═══════════╦══════════════╦═══════════════════╗
+// ║ PRESCALER ║ TIMER_COUNTS ║ Overflow Interval ║
+// ╠═══════════╬══════════════╬═══════════════════╣
+// ║        64 ║          250 ║              1 ms ║
+// ║       256 ║          125 ║              2 ms ║
+// ║       256 ║          250 ║              4 ms ║
+// ║      1024 ║          125 ║              8 ms ║
+// ║      1024 ║          250 ║             16 ms ║
+// ╚═══════════╩══════════════╩═══════════════════╝
+const PRESCALER: u32 = 256;
+const TIMER_COUNTS: u32 = 125;
+
+const MILLIS_INCREMENT: u32 = PRESCALER * TIMER_COUNTS / 16000;
+
+static MILLIS_COUNTER: Mutex<Cell<u32>> = Mutex::new(Cell::new(0));
+
+pub fn millis_init(tc0: TC0) {
+    // Configure the timer for the above interval (in CTC mode)
+    // and enable its interrupt.
+    tc0.tccr0a.write(|w| w.wgm0().ctc());
+    tc0.ocr0a.write(|w| unsafe { w.bits(TIMER_COUNTS as u8) });
+    tc0.tccr0b.write(|w| match PRESCALER {
+        8 => w.cs0().prescale_8(),
+        64 => w.cs0().prescale_64(),
+        256 => w.cs0().prescale_256(),
+        1024 => w.cs0().prescale_1024(),
+        _ => panic!(),
+    });
+    tc0.timsk0.write(|w| w.ocie0a().set_bit());
+
+    // Reset the global millisecond counter
+    interrupt::free(|cs| {
+        MILLIS_COUNTER.borrow(cs).set(0);
+    });
+}
+
+#[avr_device::interrupt(atmega2560)]
+#[allow(non_snake_case)]
+fn TIMER0_COMPA() {
+    interrupt::free(|cs| {
+        let counter_cell = MILLIS_COUNTER.borrow(cs);
+        let counter = counter_cell.get();
+        counter_cell.set(counter + MILLIS_INCREMENT);
+    })
+}
+
+pub fn millis() -> u32 {
+    interrupt::free(|cs| MILLIS_COUNTER.borrow(cs).get())
+}
