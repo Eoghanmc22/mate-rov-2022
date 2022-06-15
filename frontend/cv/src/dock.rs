260e0d0a3f63620a1d33f772e848d443a965dc15
Eoghanmc22 <31516877+Eoghanmc22@users.noreply.github.com>
2022-06-14 21:45:36 -0500

    Correct auto dock (#26)
    Correct auto docking task

--- frontend/cv/src/dock.rs
+++ frontend/cv/src/dock.rs
@@ -2,15 +2,15 @@ use opencv::prelude::*;
 use common::controller::VelocityData;
 use crate::OpenCvHandler;
 
-pub struct AutoUp;
+pub struct AutoDock;
 
-impl OpenCvHandler for AutoUp {
+impl OpenCvHandler for AutoDock {
     fn handle_frame(&mut self, _frame: &Mat) -> anyhow::Result<(VelocityData, String)> {
         Ok((VelocityData {
-            forwards_left: 0.0,
-            forwards_right: 0.0,
+            forwards_left: 1.0,
+            forwards_right: 1.0,
             strafing: 0.0,
-            vertical: 1.0
-        }, "Going up!".to_owned()))
+            vertical: 0.0
+        }, "Docking".to_owned()))
     }
 }
